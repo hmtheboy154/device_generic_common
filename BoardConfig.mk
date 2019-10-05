4d24f25aac57e143350846c63977c55943e6733c
Michael Goffioul <michael.goffioul@lincor.com>
2019-10-04 20:56:44 -0400

    Generate system root image, no ramdisk
    The generated system partition is expected to be mounted at / and
    includes the former ramdisk (from $OUT/root directory).
    
    Conflicts:
    	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,9 +109,8 @@ DEVICE_MANIFEST_FILE := device/generic/common/manifest.xml
 
 BOARD_SEPOLICY_DIRS += device/generic/common/sepolicy \
                        system/bt/vendor_libs/linux/sepolicy \
-SELINUX_IGNORE_NEVERALLOWS := true
-# Vendor Interface Manifest
-# DEVICE_MATRIX_FILE := device/generic/common/compatibility_matrix.xml
+                       
+BOARD_BUILD_SYSTEM_ROOT_IMAGE:= true
 
 # Bliss optimizations
 WITH_BLISS_CHARGER := false
