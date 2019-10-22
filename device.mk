9f31257008383dfcee1cf60fd450b99621bf55d5
Jon West <electrikjesus@gmail.com>
2019-10-22 00:01:04 -0400

    Define minimum PRODUCT_SHIPPING_API_LEVEL
    Change-Id: I91fb1612ce49c04a8c4f3a8ede0d98f959f7f3c5

--- device.mk
+++ device.mk
@@ -68,6 +68,8 @@ PRODUCT_PROPERTY_OVERRIDES := \
     media.sf.omx-plugin=libffmpeg_omx.so \
     media.sf.extractor-plugin=libffmpeg_extractor.so
 
+PRODUCT_SHIPPING_API_LEVEL := 19
+
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
     ro.arch=x86 \
     persist.rtc_local_time=1 \
