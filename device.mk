ad602bfc0ed65a12542270d6278b53ed50d868cb
Unknown <electrikjesus@gmail.com>
2019-11-23 14:37:59 -0500

    Comment out custom native-bridge for now

--- device.mk
+++ device.mk
@@ -189,7 +189,7 @@ $(call inherit-product-if-exists,$(LOCAL_PATH)/nativebridge/nativebridge.mk)
 $(call inherit-product,$(if $(wildcard $(PRODUCT_DIR)packages.mk),$(PRODUCT_DIR),$(LOCAL_PATH)/)packages.mk)
 
 # Get proprietary files if any exists
-$(call inherit-product,vendor/google/chromeos-x86/target/native_bridge_arm_on_x86.mk)
+# $(call inherit-product,vendor/google/chromeos-x86/target/native_bridge_arm_on_x86.mk)
 $(call inherit-product,vendor/google/chromeos-x86/target/houdini.mk)
 $(call inherit-product,vendor/google/chromeos-x86/target/widevine.mk)
 
