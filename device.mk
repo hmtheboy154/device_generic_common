df01a991d1e14e3decd851c98b77f96621c8dffd
AXIM0S <aminurfisa3@gmail.com>
2020-08-08 18:05:38 -0400

    Add Gearlock to device.mk
    Change-Id: I67099c3e2ae1883085fcc5e1cf22a6c5d3088f9c

--- device.mk
+++ device.mk
@@ -248,6 +248,11 @@ PRODUCT_COPY_FILES += \
 #     media.amplayer.widevineenable=true
 
 
+# Gearlock 
+PRODUCT_COPY_FILES += \
+    vendor/gearlock/gearlock:gearlock
+
+
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_CHARACTERISTICS := tablet
